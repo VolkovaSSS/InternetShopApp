ab7a8823ce1d5c84e92ed05bcb6acb23736a3fc8
Sv <VolkovaSVG@mail.ru>
2026-03-03 00:11:07 +0400

    Шаблоны, контроллеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", ),
+    path("home/", views.home, name='home' ),
+    path('contact/', views.contact, name='contact'),
     ]
