e0b708041af8020ead5f98f0413cffa42e6ce2e2
Sv <VolkovaSVG@mail.ru>
2026-03-03 00:22:40 +0400

    В репозиторий добавлены файлы, созданные при установке Jango

---
+++ catalog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    """ Страница Home"""
+
+    return render(request, 'home.html')
+
+
+def contact(request):
+    """ Страница Контакты"""
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contacts.html')
