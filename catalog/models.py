f8560b2c6080b8a6d42bbecae01fc1ef4e19d1c1
Sv <VolkovaSVG@mail.ru>
2026-03-06 00:46:19 +0400

    migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from unicodedata import category
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -11,7 +25,11 @@ class Product(models.Model):
         verbose_name="Изображение",
     )
     category = models.ForeignKey(
-        on_delete=models.SET_NULL, blank=True, null=True, related_name="products"
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="products",
     )
     price = models.FloatField(verbose_name="Цена")
     created_at = models.DateField(blank=True, null=True, verbose_name="Дата создания")
@@ -21,19 +39,6 @@ class Product(models.Model):
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ["category", "name"]
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name="Наименование")
-    description = models.TextField(verbose_name="Описание")
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-        ordering = ["name"]
 
     def __str__(self):
         return self.name
