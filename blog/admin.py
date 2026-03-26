39ca89fce006a4f6684b5297a116c9ab9baefc3f
Sv <VolkovaSVG@mail.ru>
2026-03-26 18:59:57 +0400

    Блог: миграции

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,8 @@ from blog.models import BlogRecord
 
 
 @admin.register(BlogRecord)
-class ProductAdmin(admin.ModelAdmin):
+class BlogRecordAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "created_at", "is_published")
-    # list_filter = ("category",)
-    search_fields = ("title")
-    readonly_fields = ("created_at")
+    list_filter = ("is_published",)
+    search_fields = ("title",)
+    readonly_fields = ("created_at",)
